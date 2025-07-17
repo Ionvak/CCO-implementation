081106cd6987897ebdbb8a7f6d0aba6b11495da4
Mahmoud Elshekh Ali <mmahmoudelshekhali@gmail.com>
2025-07-17 22:32:43 +0200

    Added tree initialization parameters to 'Tree' class.

--- src/com/cco/Tree.java
+++ src/com/cco/Tree.java
@@ -1,7 +1,15 @@
 package com.cco;
-import com.cco.Point;
-import com.cco.Segment;
+
+/*This class represents the whole tree. It is intended to provide the full interface for
+ *building and interacting with the tree.
+ * */
 
 public class Tree {
+
+    private static double viscosity;
+    private static double distalPressure;
+    private static double perfusionPressure;
+    private static double N_terminal;
+    private static double bifurcationExponent;
 
 }
