718cc86b641c11ec59afc6c97ae1a997597cf1ae
Mahmoud Elshekh Ali <mmahmoudelshekhali@gmail.com>
2025-07-21 22:25:16 +0200

    Removed id attribute from the "Point" class. Added getters for each field in this class.

--- src/com/cco/Point.java
+++ src/com/cco/Point.java
@@ -4,15 +4,19 @@ package com.cco;
 
 class Point {
 
-    private static long ID;
-    final long id; //id of the point
     private double x; //x coordinate of the point
     private double y; //y coordinate of the point
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
-        this.id = ID++;
     }
 
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
 }
