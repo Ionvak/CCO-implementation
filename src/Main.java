13eea453e82fa987484baaefed47d8d862963f0a
Mahmoud Elshekh Ali <mmahmoudelshekhali@gmail.com>
2025-07-22 22:09:20 +0200

    Code defining process of tree creation and display.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
+        double viscosity = 0;
+        double bifurcationExponent = 0;
+        double distalPressure = 0;
+        double perfusionPressure = 0;
+        double perfusionFlow = 0;
+        double perfusionArea = 0;
+        int N_terminal = 0;
 
-    }
+        ArterialTree newTree = new ArterialTree(viscosity,bifurcationExponent,distalPressure,perfusionPressure,perfusionFlow,perfusionArea,N_terminal);
+        newTree.displayTree();
+        }
 }
