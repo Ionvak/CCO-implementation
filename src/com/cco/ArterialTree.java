8fce38aeb14e5ffbd9d6ed5a36f5b1d6f8b18fd2
Mahmoud Elshekh Ali <mmahmoudelshekhali@gmail.com>
2025-07-23 21:12:57 +0200

    Added comments to interface. Changed access specifier for root field.

--- src/com/cco/ArterialTree.java
+++ src/com/cco/ArterialTree.java
@@ -18,7 +18,7 @@ public class ArterialTree extends HashMap<Long, Segment> {
     final int N_terminal;
 
     //Process variables for the tree
-    private Segment root;
+    final Segment root;
     private double supportRadius;
     private int k_term;
     private int k_tot;
@@ -32,6 +32,8 @@ public class ArterialTree extends HashMap<Long, Segment> {
         this.perfusionFlow = perfusionFlow;
         this.perfusionArea = perfusionArea;
         this.N_terminal = N_terminal;
+
+        //Root proximal and distal should be randomly selected
         this.root = new Segment(0,0,1,1,perfusionFlow/N_terminal,viscosity,perfusionPressure,distalPressure);
 
         while(k_term != N_terminal){
@@ -39,50 +41,64 @@ public class ArterialTree extends HashMap<Long, Segment> {
         }
     }
 
+    //Used exclusively for testing
     Segment getRoot(){
         return this.root;
     }
 
+    //Helper method for getting the threshold distance
     static private double thresholdDistance(){
 
         return 0;
     }
 
+    //Helper method for stretching the supporting circle. Returns true if successful, false otherwise
     static private boolean supportCircleStretch(){
 
         return false;
     }
 
+    //Helper method for adding a bifurcation, and thus a new segment, into the tree. returns the
+    //index of the added segment if successful, 0 otherwise
     static private long addBifurcation(){
 
         return 0;
     }
 
+    //Helper method for testing a bifurcation from among the viable candidates. Returns the blood volume
+    //after geometric optimization
     static private double testBifurcation(){
 
         return 0;
     }
 
+    //Helper method for deciding if the projection distance (d_proj) is such that orthogonal distance
+    //is critical
     static private boolean findProjection(){
 
         return false;
     }
 
+    //Helper method for getting the orthogonal distance
     static private double orthogonalDistance(){
 
         return 0;
     }
 
+    //Helper method for getting the distance between the new location and one of the endpoints of
+    //an already existing segment
     static private double endpointDistance(){
 
         return 0;
     }
 
+    //Helper method for rescaling the tree
     static private boolean rescaleTree(){
 
         return false;
     }
 
+    //Used for getting a graphical representation of the tree
     public void displayTree(){
         //code for displaying the tree
     }
