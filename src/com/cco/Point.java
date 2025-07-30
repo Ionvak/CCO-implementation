e4dca64a3e1b5954b70ef2350aaf2917a7454d6f
Mahmoud Elshekh Ali <mmahmoudelshekhali@gmail.com>
2025-07-30 15:49:03 +0200

    Added back ID field to "Point" class. Changed names of methods in "Segment" class. Removed static keyword from distance method.

--- src/com/cco/Point.java
+++ src/com/cco/Point.java
@@ -3,11 +3,13 @@ package com.cco;
 /*This class represents a Point within a 2D coordinate space*/
 
 class Point {
-
+    static private long ID = 0;
+    long id;
     double x; //x coordinate of the point
     double y; //y coordinate of the point
 
     public Point(double x, double y) {
+        id = ID++;
         this.x = x;
         this.y = y;
     }
