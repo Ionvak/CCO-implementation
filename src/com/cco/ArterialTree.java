6b3d44652728dfdc999282c11a07d92ea2d48050
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-10 21:08:21 +0200

    Implemented getTarget and treeDetails for getting the target function value and displaying tree parameters respectively in ArterialTree class. Added target field in ArterialTree class to store target func value.

--- src/com/cco/ArterialTree.java
+++ src/com/cco/ArterialTree.java
@@ -8,15 +8,17 @@ import java.util.HashMap;
 
 public class ArterialTree{
 
-    final HashMap<Integer, Segment> body;
-    final TreeParams params;
+    private final HashMap<Integer, Segment> body;
+    private final TreeParams params;
     private boolean isBuilt;
+    private double target;
 
     //Initialize the tree given the constants passed to the constructor
     public ArterialTree(TreeParams parameters) {
         this.params = parameters;
         this.body = new HashMap<Integer, Segment>();
         this.isBuilt = false;
+        this.target = 0;
     }
 
     public void buildTree(){
@@ -24,7 +26,45 @@ public class ArterialTree{
     }
 
     public double getTarget(){
-        return 0;
+        if(!this.isBuilt) return 0;
+        double sum = 0;
+        for(Segment s: body.values()) {
+            sum += s.findVolume();
+        }
+        return sum;
+    }
+
+    public void treeDetails(){
+        if(!this.isBuilt){
+            System.out.println("Tree is not built. Nothing to display.");
+            return;
+        };
+
+        System.out.println("Segments:");
+        for(Segment s: body.values()){
+            System.out.println(s.index + ":");
+            String segString = """
+                    Proximal:   %s,
+                    Distal:     %s,
+                    Length:     %f,
+                    Radius:     %f,
+                    Parent:     %d,
+                    Left Child: %d,
+                    Right Child:%d,
+                    """;
+            String result = String.format(
+                            s.proximal.toString(),
+                            s.distal.toString(),
+                            s.length,
+                            s.radius,
+                            s.parent.index,
+                            s.childLeft.index,
+                            s.childRight.index
+            );
+
+            System.out.println(result);
+            System.out.println("Target function value: " + target);
+        }
     }
 
 }
