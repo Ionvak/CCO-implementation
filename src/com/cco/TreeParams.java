8dabe474b6badcea6a95911a40b7c577058a8c04
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-10 22:21:45 +0200

    Added perfRadius field to TreeParams class representing perfusion area radius.

--- src/com/cco/TreeParams.java
+++ src/com/cco/TreeParams.java
@@ -7,15 +7,17 @@ public class TreeParams {
     final double perfPress;
     final double perfFlow;
     final double perfArea;
+    final double perfRadius;
     final int nTerminal;
 
-    public TreeParams(double viscosity, double bifExponent, double distalPress, double perfPress, double perfFlow, double perfArea, int numTerminal) {
+    public TreeParams(double viscosity, double bifExponent, double distalPress, double perfPress, double perfFlow, double perfArea, double perfRadius, int numTerminal) {
         this.viscosity = viscosity;
         this.bifExponent = bifExponent;
         this.distalPress = distalPress;
         this.perfPress = perfPress;
         this.perfFlow = perfFlow;
         this.perfArea = perfArea;
+        this.perfRadius = perfRadius;
         this.nTerminal = numTerminal;
     }
 }
