5bc722a3dca83a0850ba73418c6d4ea52ec5822e
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-10 17:49:36 +0200

    Added SupportingCircle class to separate build logic for tree from its intended UI functionality

---
+++ src/com/cco/SupportingCircle.java
@@ -0,0 +1,10 @@
+package com.cco;
+
+public class SupportingCircle {
+    private double supportRadius;
+    private double SupportArea;
+    private double threshDistance;
+    private int kTerminal;
+    private int kTotal;
+    private int nToss;
+}
