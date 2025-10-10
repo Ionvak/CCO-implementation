2cf150a69c28c8b97a047d96c691aa2aaa62f7af
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-10 17:50:33 +0200

    Added TreeParams class to avoid long parameter lists for tree methods.

---
+++ src/com/cco/TreeParams.java
@@ -0,0 +1,21 @@
+package com.cco;
+
+public class TreeParams {
+    final double viscosity;
+    final double bifExponent;
+    final double distalPress;
+    final double perfPress;
+    final double perfFlow;
+    final double perfArea;
+    final int numTerminal;
+
+    public TreeParams(double viscosity, double bifExponent, double distalPress, double perfPress, double perfFlow, double perfArea, int numTerminal) {
+        this.viscosity = viscosity;
+        this.bifExponent = bifExponent;
+        this.distalPress = distalPress;
+        this.perfPress = perfPress;
+        this.perfFlow = perfFlow;
+        this.perfArea = perfArea;
+        this.numTerminal = numTerminal;
+    }
+}
