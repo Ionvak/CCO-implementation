82e6aed188499024a3d9d195c27d0fddeb0e4e4d
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-11 16:32:51 +0200

    Removed unnecessary fields perfArea and termFlow from treeParams class.

--- src/com/cco/TreeParams.java
+++ src/com/cco/TreeParams.java
@@ -11,20 +11,16 @@ public class TreeParams {
     final double distalPress;
     final double perfPress;
     final double perfFlow;
-    final double perfArea;
     final double perfRadius;
-    final double termFlow;
     final int nTerminal;
 
-    public TreeParams(double viscosity, double bifExponent, double distalPress, double perfPress, double perfFlow, double perfArea, double perfRadius, double termFlow, int numTerminal) {
+    public TreeParams(double viscosity, double bifExponent, double distalPress, double perfPress, double perfFlow, double perfArea, double perfRadius, double termFlow, int nTerminal) {
         this.viscosity = viscosity;
         this.bifExponent = bifExponent;
         this.distalPress = distalPress;
         this.perfPress = perfPress;
         this.perfFlow = perfFlow;
-        this.perfArea = perfArea;
         this.perfRadius = perfRadius;
-        this.termFlow = termFlow;
-        this.nTerminal = numTerminal;
+        this.nTerminal = nTerminal;
     }
 }
