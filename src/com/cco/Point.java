089ed6b2af78550266e97fad92e2658307bca771
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-11 13:23:51 +0200

    Added id field to Point class

--- src/com/cco/Point.java
+++ src/com/cco/Point.java
@@ -3,10 +3,13 @@ package com.cco;
 /*This class represents a Point within a 2D coordinate space*/
 
 class Point {
+    private static long ID = 0;
+    final long id;
     double x; //x coordinate of the point
     double y; //y coordinate of the point
 
     public Point(double x, double y) {
+        this.id = ID++;
         this.x = x;
         this.y = y;
     }
