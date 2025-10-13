b99aa3729afd24d84aec410e9d9eb0a47eee20f4
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-13 16:06:38 +0200

    Removed point IDs as they added no value.

--- src/com/cco/Point.java
+++ src/com/cco/Point.java
@@ -3,13 +3,10 @@ package com.cco;
 /*This class represents a Point within a 2D coordinate space*/
 
 class Point {
-    private static long ID = 1; //Variable used to generate IDs.
-    final long id; //ID of the point.
     double x; //x coordinate of the point
     double y; //y coordinate of the point
 
     public Point(double x, double y) {
-        this.id = ID++;
         this.x = x;
         this.y = y;
     }
