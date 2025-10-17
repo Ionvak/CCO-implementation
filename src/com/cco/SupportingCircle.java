bc8d6efa0dffe5e6ee494462c690c87cf2c6e6b2
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-10-17 16:43:18 +0200

    Moved getTarget to be part of ArterialTree class interface. Added basic bifurcation addition. Implemented tree stretching.

--- src/com/cco/SupportingCircle.java
+++ src/com/cco/SupportingCircle.java
@@ -21,18 +21,16 @@ public class SupportingCircle {
         this.threshDistance = Math.sqrt(this.supportArea / this.kTerm);
     }
 
-    private Point toss(TreeParams parameters){
+     Point toss(double perfRadius){
         Random rand = new Random();
-        double perfRadius = parameters.perfRadius;
         double x = rand.nextDouble() * (2 * perfRadius) -  perfRadius;
         double circleBorder = Math.sqrt(Math.pow(perfRadius, 2) - Math.pow(x, 2));
         double y = rand.nextDouble() * (2 * circleBorder) -  circleBorder;
-
         return new Point(x,y);
     }
 
     private double findCritDistance(Segment segment, Point point){
-        double dCrit = 0;
+        double dCrit;
         double dProjection = findProjection(segment, point);
         if(0 <= dProjection && dProjection <=1)
             dCrit = findOrthogonal(segment, point);
@@ -67,36 +65,64 @@ public class SupportingCircle {
         );
     }
 
-    private double findRadius(TreeParams parameters, double length, double pressDiff, double flow){
-        return Math.pow(
-                (8 * parameters.viscosity * length * flow) /
-                (Math.PI * pressDiff)
-                ,0.25);
+    private void stretchSegments(Segment s, double factor, double aggregateX,  double aggregateY){
+        double deltaX = aggregateX;
+        double deltaY = aggregateY;
+
+        s.proximal.x += deltaX;
+        s.proximal.y += deltaY;
+
+        deltaX += (s.distal.x - s.proximal.x) * factor;
+        deltaY += (s.distal.y - s.proximal.y) * factor;
+
+        s.distal.x += deltaX;
+        s.distal.y += deltaY;
+
+        s.length = Segment.findLength(s.proximal, s.distal);
+        //double flow = Segment.findFlow();
+        //double pressDiff = ;
+        //s.radius = Segment.findRadius(parameters, s.length, pressDiff, flow);
+
+        if(s.childLeft != null) stretchSegments(s.childLeft, factor, deltaX, deltaY);
+        if(s.childRight != null) stretchSegments(s.childRight, factor, deltaX, deltaY);
     }
 
-    private void findOptimal(HashMap<Long, Segment> tree){
+     double addBif(HashMap<Long, Segment> tree, Segment iConn, Point iNewDistal, boolean keepChanges){
+
+        Point iConnDistPrev = new Point(iConn.distal.x, iConn.distal.y);
+        Point iConnProxPrev = new Point(iConn.proximal.x, iConn.proximal.y);
+
+        iConn.distal.x = iConn.proximal.x + 0.5 * (iConn.distal.x - iConn.proximal.x);
+        iConn.distal.y = iConn.proximal.y + 0.5 * (iConn.distal.y - iConn.proximal.y);
+        iConn.length = Segment.findLength(iConn.proximal, iConn.distal);
+        iConn.proximal = iConn.distal;
+        iConn.distal = iConnDistPrev;
+
+        Segment iBif = new Segment(iConnProxPrev, iConn.proximal, 0);
+        Segment iNew = new Segment(iBif.distal, iNewDistal, 0);
+        iBif.parent = iConn.parent;
+        iConn.parent = iBif;
+        iNew.parent = iBif;
+        iBif.childLeft = iBif.distal.x > iNew.distal.x ? iNew : iConn;
+        iBif.childRight = iBif.distal.x <= iNew.distal.x ? iNew : iConn;
+        tree.put(iBif.index,  iBif);
+        tree.put(iNew.index,  iNew);
+
+        //readjust parameters
+        //rescale tree
+        return 0;
+    }
+
+    private void rescaleTree(HashMap<Long, Segment> tree){
 
     }
 
-    //Calculate and return the target function value for the tree
-    double getTarget(HashMap<Long, Segment> tree){
-        double sum = 0;
-        for(Segment s: tree.values()) {
-            sum += Segment.findVolume(s.radius, s.length);
-        }
-        return sum;
-    }
 
     /*
     ====================================================================================================================
      Beginning of interface between ArterialTree and SupportingCircle class
     */
 
-    /**
-     *
-     * @param segments: Hashmap of all segments in the arterial tree.
-     * @param parameters: Physical parameters of the tree.
-     */
     void initRoot(HashMap<Long, Segment> segments, TreeParams parameters){
         Random rand = new Random();
         double perfRadius = parameters.perfRadius;
@@ -111,7 +137,7 @@ public class SupportingCircle {
         double critDistance = 0;
         double threshDist = this.threshDistance;
         while(!distalFound){
-            rootDistal = toss(parameters);
+            rootDistal = toss(parameters.perfRadius);
             loopCount++;
             if(loopCount == nToss){
                 threshDist -= this.threshDistance * 0.1;
@@ -125,32 +151,39 @@ public class SupportingCircle {
         double length = Segment.findLength(rootProximal, rootDistal);
         double pressDiff = parameters.perfPress - parameters.distalPress;
         double flow = parameters.perfFlow;
-        double radius = findRadius(parameters, length, pressDiff, flow);
+        double radius = Segment.findRadius(parameters, length, pressDiff, flow);
 
         Segment root = new Segment(rootProximal, rootDistal, radius);
         segments.put(root.index, root);
     }
 
-    void stretchCircle(HashMap<Long, Segment> tree){
+    //Calculate and return the target function value for the tree
+    double getTarget(HashMap<Long, Segment> tree){
+        double sum = 0;
+        for(Segment s: tree.values()) {
+            sum += Segment.findVolume(s.radius, s.length);
+        }
+        return sum;
+    }
+
+    void stretchCircle(Segment root, TreeParams parameters){
+        double prevArea = this.supportArea;
+        this.supportArea = (this.kTot + 1) * Math.pow(parameters.perfRadius, 2) / parameters.nTerminal;
+        this.supportRadius = Math.sqrt(this.supportArea / Math.PI);
+        double factor = ( this.supportArea / prevArea ) - 1;
+        this.threshDistance = Math.sqrt(this.supportArea / this.kTerm);
+        stretchSegments(root, factor, 0, 0);
+    }
+
+    private void addBifOptimal(HashMap<Long, Segment> tree){
 
     }
 
-    double addBif(HashMap<Long, Segment> tree, boolean keepChanges){
-        return 0;
-    }
-
-
-    void rescaleTree(HashMap<Long, Segment> tree){
-
-    }
 
     /*
     TODO:
-     - Implement bifurcation addition and more generally
-        improve on tree building procedure.
+     - Implement bifurcation addition.
      - Implement procedure for finding the optimal bifurcation.
-     - Implement supporting tree stretching.
      - Implement tree rescaling after bifurcation addition.
-     - Move getTarget to SupportingCircle and name it findTarget
      */
 }
