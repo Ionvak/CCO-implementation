20e979de89ff85b52e7cedf404b9f22ab587c181
melshekh <mmahmoudelshekhali@gmail.com>
2026-02-21 15:58:00 +0100

    Added section for series exportation and removed outdated visualization tools.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import com.cco.ArterialTree;
 import com.cco.TreeParams;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.XYChart;
-import org.knowm.xchart.XYChartBuilder;
-import org.knowm.xchart.XYSeries;
-import org.knowm.xchart.style.markers.SeriesMarkers;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import java.awt.*;
 
@@ -18,38 +15,22 @@ void main() {
             8.38e3, //termPress
             8.33e-6,//perfFlow
             0.05,   //perfRadius
-            250     //numTerminal
+            8     //numTerminal
     );
     ArterialTree arterialTree = new ArterialTree(params);
     arterialTree.buildTree();
     arterialTree.treeDetails();
 
-    //Build and style chart
-    XYChart chart = new XYChartBuilder().width(800).height(600).build();
-    chart.getStyler().setChartTitleVisible(false);
-    chart.getStyler().setLegendVisible(false);
-    chart.getStyler().setXAxisMin(-0.05);
-    chart.getStyler().setXAxisMax(0.05);
-    chart.getStyler().setYAxisMin(-0.05);
-    chart.getStyler().setYAxisMax(0.05);
-
-    //Plot perfusion area
-    double[][] perfCircle = arterialTree.getPerfArea();
-    XYSeries circle = chart.addSeries("Perfusion Area", perfCircle[0], perfCircle[1]);
-    circle.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
-    circle.setLineColor(Color.BLACK);
-    circle.setMarker(SeriesMarkers.NONE);
-
-    //Plot the tree
+    //Export tree
     double[][] series = arterialTree.getSeries();
-    XYSeries segments = chart.addSeries("tree", series[0], series[1]);
-    segments.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
-    segments.setMarker(SeriesMarkers.CIRCLE);
-    segments.setLineColor(Color.RED);
-    segments.setMarkerColor(Color.YELLOW);
-
-
-    //Show the chart
-    new SwingWrapper(chart).displayChart();
-
+    try (FileWriter exportWriter = new FileWriter("src/tree_data.txt")) {
+        exportWriter.write(Arrays.toString(series[0]) +
+                               "\n" +
+                               Arrays.toString(series[1]));
+        System.out.println("Successfully wrote to the file.");
+    }
+    catch (IOException e){
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
 }
