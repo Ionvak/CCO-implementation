cb2e338cc6bbb93d030dfcedeb5abbe03a45cd0b
melshekh <mmahmoudelshekhali@gmail.com>
2026-03-05 22:17:19 +0100

    Moved saving logic to saveState method into ArterialTree class.

--- src/Main.java
+++ src/Main.java
@@ -15,22 +15,10 @@ void main() {
             8.38e3, //termPress
             8.33e-6,//perfFlow
             0.05,   //perfRadius
-            4     //numTerminal
+            4000      //numTerminal
     );
+
     ArterialTree arterialTree = new ArterialTree(params);
     arterialTree.buildTree();
     arterialTree.treeDetails();
-
-    //Export tree
-    double[][] series = arterialTree.getSeries();
-    try (FileWriter exportWriter = new FileWriter("src/tree_data.txt")) {
-        exportWriter.write(Arrays.toString(series[0]) +
-                               "\n" +
-                               Arrays.toString(series[1]));
-        System.out.println("Successfully wrote to the file.");
-    }
-    catch (IOException e){
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-    }
 }
