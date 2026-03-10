fb294c39d2cd154385c89fbbcc53c2ddfd81105c
melshekh <mmahmoudelshekhali@gmail.com>
2026-03-10 20:59:00 +0100

    Updated visualization.py script to work with JSON

--- src/visualization.py
+++ src/visualization.py
@@ -1,56 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import re
 import math
+import json
 
-# Enter the filepath to the "tree_data.json" file from the source code folder
+# Enter the filepath to the "tree_data.txt" file from the source code folder
 filepath = 'src/tree_data.json'
-# Enter the perfusion radius (must be equivalent to the related attribute
-# in the parameters of the tree)
-perfRadius = 0.05
+
 # Enter the max value of the linewidth (used for the root radius).
 line_width_range = 4
+
+# Open the "tree_data.json" file from the source code and read the exported tree
+with open(filepath, 'r') as file:
+    tree = json.load(file)["ArterialTree"]
+
+params = tree["params"]
+segments = tree["segments"]
+num_of_segments = len(segments)
+num_of_terminals = (num_of_segments+1)//2
+
+# Find the maximum radius. Used for normalizing the radii values.
+max_radius = 0
+for s in segments.values():
+    if(s["radius"] > max_radius): max_radius = s["radius"]
 
 # Generate a set of points representing the circle representing the
 # Perfusion area.
 PRECISION = 300
+perf_radius = params["perfRadius"]
 phi_step = 2 * math.pi / PRECISION
 xarea = list()
 yarea = list()
 for i in range(0, 2 * PRECISION, 2):
     phi = i * phi_step
-    xarea.append(perfRadius * math.cos(phi))
-    yarea.append(perfRadius * math.sin(phi))
+    xarea.append(perf_radius * math.cos(phi))
+    yarea.append(perf_radius * math.sin(phi))
 xarea = np.array(xarea)
 yarea = np.array(yarea)
 
-# Open the "tree_data.json" file from the source code and read the exported
-# segment attributes. "x" is an array of the x coordinates of all the segment
-# points and radii. "y" is an array of the y coordinates of all the segment points.
-with open(filepath, 'r') as file:
-    for i in range(4):
-        xline = file.readline()
-        yline = file.readline()
-        xseries = re.findall(r"[-]?\d+[.]\d+(?:[eE][+-]\d+)?", xline)
-        yseries = re.findall(r"[-]?\d+[.]\d+(?:[eE][+-]\d+)?", yline)
-        x = [float(a) for a in xseries]
-        y = [float(a) for a in yseries]
-        num_of_segments = len(x)//3
-        num_of_terminals = (num_of_segments + 1)//2
-
-        # Find the maximum radius. Used for normalizing the radii values.
-        max_radius = 0
-        for n in range(0,len(x),3):
-            if x[n+2] > max_radius:
-                max_radius = x[n+2]
-
-        # Plot the segments one by one. Adjust the linewidth according to the
-        # segment radius. Afterwards, Plot the perfusion circle.
-        plt.subplot(2,2,i+1)
-        for n in range(0,len(x),3):
-            plt.plot([x[n], x[n+1]], [y[n], y[n+1]], 'r', lw= line_width_range * (x[n+2] / max_radius))
-        plt.plot(xarea, yarea, '-k')
-        plt.axis('equal')
-        plt.title(str(num_of_terminals) + " Terminal Segments")
+# Plot the segments one by one. Adjust the linewidth according to the
+# segment radius. Afterwards, Plot the perfusion circle.
+#plt.subplot(2,2,i+1)
+for s in segments.values():
+    plt.plot( [s["proximal"][0], s["distal"][0]],
+              [s["proximal"][1], s["distal"][1]],
+              'r',
+              lw= line_width_range * (s["radius"] / max_radius))
+plt.plot(xarea, yarea, '-k')
+plt.axis('equal')
+plt.title(str(num_of_terminals) + " Terminal Segments")
 
 plt.show()
