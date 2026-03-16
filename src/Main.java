e2568241c76de1da1e2fabf3ec68e0f8d9b9986b
melshekh <mmahmoudelshekhali@gmail.com>
2026-03-16 20:11:37 +0100

    Added some basic functionality for measuring execution time.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.cco.ArterialTree;
 import com.cco.TreeParams;
 
 void main() {
-
+    long startTime = System.nanoTime();
     //Build tree
     TreeParams params = new TreeParams(
             3.6e-3, //viscosity
@@ -17,4 +17,18 @@ void main() {
     ArterialTree arterialTree = new ArterialTree(params);
     arterialTree.buildTree();
     arterialTree.treeDetails();
+
+    long endTime = System.nanoTime();
+    long seconds = (endTime - startTime) / 1000000000;
+    long minutes = 0;
+    long hours = 0;
+    if(seconds >= 60){
+        minutes = seconds / 60;
+        seconds = seconds % 60;
+        if(minutes >= 60){
+            hours = minutes / 60;
+            minutes = minutes % 60;
+        }
+    }
+    System.out.println("Execution Time: " + hours + "h " + minutes + "m " + seconds + "s");
 }
